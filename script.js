8f132678bda6f312834a8a7905df27a4565c7c66
DevCodeX <countryboya20@gmail.com>
2023-09-09 15:52:33 -0500

    Career Website

---
+++ script.js
@@ -0,0 +1,15 @@
+const myWhyText = document.getElementById("myWhyText");
+const myWhyButton = document.getElementById("myWhyButton");
+
+myWhyButton.addEventListener("click", () => {
+    myWhyText.classList.toggle("animate");
+
+    if (myWhyText.classList.contains("animate")) {
+      myWhyText.style.display = "block";
+
+    }else{
+     myWhyText.style.display = "none";
+    }
+
+
+});
